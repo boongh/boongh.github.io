54ec73c6ac2068466342ee64d564dc917f0e1731
Boon <boonsvj2016@gmail.com>
2025-08-27 02:52:04 +0100

    Complete main functionality for letter boxed

---
+++ apps/router/src/homepage/main.jsx
@@ -0,0 +1,9 @@
+function App(){
+
+    return <div
+    className="w-screen h-screen bg-gradient-to-b from-indigo-300 to-pink-300">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa adipisci eligendi omnis, odio sapiente rem at provident facere eos harum, suscipit excepturi delectus magni error. Voluptatum autem amet ut quam?</p>
+    </div>
+}
+
+export default App
